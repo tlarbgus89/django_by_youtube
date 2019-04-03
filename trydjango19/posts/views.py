657e8263af944a05ec2e01582df3d238fd49ee14
Kyuhyeon Sim <tlarbgus89@gmail.com>
2019-04-04 03:22:21 +0900

    incresed searching speed

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -9,22 +9,60 @@ from .models import Post
 
 
 
-### 추가한부분
+# ### 추가한부분 ########################################################
+# def contact(request):
+# 	# with open("/Users/sim_macbookpro/Desktop/File0.txt", 'r') as f:
+# 	# 	data = f.readlines()
+# 	# title_sub = []
+# 	# results = []
+# 	# title_sub2 = []
+# 	# results2 = []
+# 	# for title in data[:2]:
+# 	# 	title_sub.append(title.strip())
+# 	# results.append((" ".join(title_sub)).strip())
+# 	# output1 = (" ".join(results)).strip()
+# 	# for title2 in data[2:]:
+# 	# 	title_sub2.append(title2.strip())
+# 	# results2.append((" ".join(title_sub2)).strip())
+# 	# output2 = (" ".join(results2)).strip()
+#
+# 	from gensim.models import Doc2Vec
+# 	import pickle
+# 	import gensim
+#
+# 	# with open("/Users/sim_macbookpro/projects/project_judical_precednet_not_py/download_j_precednet/jp_by_4000/for_03/train_corpus_pickle","rb") as fp:
+# 	# 	train_corpus = pickle.load(fp)
+# 	#
+# 	model = Doc2Vec.load('/Users/sim_macbookpro/projects/project_judical_precednet_not_py/download_j_precednet/jp_by_4000/for_03/model_mecab_noun_79403')
+# 	# model = gensim.models.doc2vec.Doc2Vec(tratravector_size=50, min_count=2, epochs=40)
+#
+# 	#
+# 	# with open("/Users/sim_macbookpro/projects/project_judical_precednet_not_py/download_j_precednet/jp_by_4000/for_03/ranks_pickle","rb") as fp:
+# 	# 	ranks = pickle.load(fp)
+# 	#
+# 	# with open("/Users/sim_macbookpro/projects/project_judical_precednet_not_py/download_j_precednet/jp_by_4000/for_03/second_ranks_pickle","rb") as fp:
+# 	# 	second_ranks = pickle.load(fp)
+#
+#
+# 	test_corpus = ['자동차','사고', '손해배상']
+# 	inferred_vector = model.infer_vector(test_corpus)
+# 	sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+#
+# 	filenum = []
+# 	for label, index in [('MOST', 0), ('SECOND', 1), ('THIRD', 2), ('THIRD', 3), ('THIRD', 4), ('THIRD', 5), ('THIRD', 6), ('THIRD', 7), ('THIRD', 8), ('THIRD', 9), ('THIRD', 10), ]:
+# 		filenum.append(sims[index][0])
+#
+# 	posts = []
+# 	for num in filenum:
+# 		post_one = Post.objects.get(fileid = num)
+# 		posts.append(post_one)
+#
+# 	return render(request, 'basic.html', {'posts':posts})
+# #####################################################################
+
+
+### 추가한부분 ########################################################
 def contact(request):
-	# with open("/Users/sim_macbookpro/Desktop/File0.txt", 'r') as f:
-	# 	data = f.readlines()
-	# title_sub = []
-	# results = []
-	# title_sub2 = []
-	# results2 = []
-	# for title in data[:2]:
-	# 	title_sub.append(title.strip())
-	# results.append((" ".join(title_sub)).strip())
-	# output1 = (" ".join(results)).strip()
-	# for title2 in data[2:]:
-	# 	title_sub2.append(title2.strip())
-	# results2.append((" ".join(title_sub2)).strip())
-	# output2 = (" ".join(results2)).strip()
 
 	from gensim.models import Doc2Vec
 	import pickle
@@ -43,21 +81,42 @@ def contact(request):
 	# with open("/Users/sim_macbookpro/projects/project_judical_precednet_not_py/download_j_precednet/jp_by_4000/for_03/second_ranks_pickle","rb") as fp:
 	# 	second_ranks = pickle.load(fp)
 
-
-	test_corpus = ['자동차','사고', '손해배상']
+	test_corpus = ['자동차', '사고', '손해배상']
 	inferred_vector = model.infer_vector(test_corpus)
 	sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
 
 	filenum = []
-	for label, index in [('MOST', 0), ('SECOND', 1), ('THIRD', 2), ('THIRD', 3), ('THIRD', 4), ('THIRD', 5), ('THIRD', 6), ('THIRD', 7), ('THIRD', 8), ('THIRD', 9), ('THIRD', 10), ]:
+	for label, index in [('MOST', 0), ('SECOND', 1), ('THIRD', 2), ('THIRD', 3), ('THIRD', 4), ('THIRD', 5),
+						 ('THIRD', 6), ('THIRD', 7), ('THIRD', 8), ('THIRD', 9), ('THIRD', 10),('MOST', 11), ('SECOND', 12), ('THIRD', 13), ('THIRD', 14), ('THIRD', 15), ('THIRD', 16),
+						 ('THIRD', 17), ('THIRD', 18), ('THIRD', 19), ('THIRD', 20), ('THIRD', 21), ]:
 		filenum.append(sims[index][0])
 
-	posts = []
-	for num in filenum:
-		post_one = Post.objects.get(fileid = num)
-		posts.append(post_one)
-	return render(request, 'basic.html', {'posts':posts})
-###
+
+
+
+	posts1 = []
+	space = ['___________________________________________________________________________________________________________________________________________________________________']
+	similar = ['유사도']
+	space2 = ['______________________________________________________________________________________________________________________________________________________']
+
+	for i, num in enumerate(filenum):
+		with open("//Users/sim_macbookpro/projects/project_judical_precednet_not_py/download_j_precednet/jp_by_4000/total_txt/File"+ str(num) +".txt", 'r') as f:
+			data = f.readlines()
+		title_sub = []
+		title_sub2 = []
+		for title in data[:2]:
+			title_sub.append(title.strip())
+		for title2 in data[2:]:
+			title_sub2.append(title2.strip())
+		ranking = [str(i)+ '순위 : ']
+
+		posts1.append((" ".join(similar + ranking + title_sub + space + title_sub2)).strip())
+
+	return render(request, 'basic.html',{'content': posts1})
+
+
+#####################################################################
+
 
 def post_create(request):
 	form = PostForm(request.POST or None, request.FILES or None)
